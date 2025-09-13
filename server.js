97d19a06182ed5a18cc65031143d64740b54c9b6
Fabricio <fabricio.bizotto@ifc.edu.br>
2025-09-13 16:09:52 -0300

    asd

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const app = require("./src/app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
