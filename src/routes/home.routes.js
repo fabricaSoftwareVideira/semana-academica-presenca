2672e726c184ee4ce3443cb77595d2016517de40
Fabricio <fabricio.bizotto@ifc.edu.br>
2025-09-14 21:52:48 -0300

    asd

--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -5,17 +5,15 @@ const eventoController = require("../controllers/eventos.controller");
 const { ensureAuthenticated, checkRole } = require("../middlewares/auth");
 
 router.get("/", (req, res) => {
-    const user = req.user;
+    const user = req.user; // Passport preenche req.user quando logado
     const isAuthenticated = req.isAuthenticated();
-    if (isAuthenticated) {
-        return res.redirect("/dashboard");
-    }
-    // Se não estiver autenticado, mostrar a página pública com o ranking
+
     const ranking = rankingController.rankingPublico();
     const eventosPorTipo = eventoController.agruparPorTipo();
     const rankingExistePontuacao = ranking.some(u => u.pontosTotal > 0);
 
     res.render("home", { user, ranking, rankingExistePontuacao, eventosPorTipo, isAuthenticated });
 });
+
 
 module.exports = router;
