dd3924bd0ef7efb0b6894102cad83c3bcc9f1fdb
Fabricio <fabricio.bizotto@ifc.edu.br>
2025-09-16 16:45:16 -0300

    asd

--- src/repositories/evento.repository.js
+++ src/repositories/evento.repository.js
@@ -1,8 +1,13 @@
 const EventoModel = require('../models/evento.model');
 
+
 const EventoRepository = {
     getAll: () => EventoModel.getAllEventos(),
     saveAll: (eventos) => EventoModel.saveEventos(eventos),
+    findById: (id) => {
+        const eventos = EventoModel.getAllEventos();
+        return eventos.find(e => e.id === id);
+    }
 };
 
 module.exports = EventoRepository;
--- src/repositories/turma.repository.js
+++ src/repositories/turma.repository.js
@@ -3,6 +3,10 @@ const TurmaModel = require('../models/turma.model');
 const TurmaRepository = {
     getAll: () => TurmaModel.getAllTurmas(),
     saveAll: (turmas) => TurmaModel.saveTurmas(turmas),
+    findById: (id) => {
+        const turmas = TurmaModel.getAllTurmas();
+        return turmas.find(t => t.id === id);
+    }
 };
 
 module.exports = TurmaRepository;
