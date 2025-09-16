36e22ad4d134f8bcc983dbaec4fd446e4fc26b65
Fabricio <fabricio.bizotto@ifc.edu.br>
2025-09-16 13:12:31 -0300

    asd

--- src/config/passport.js
+++ src/config/passport.js
@@ -19,7 +19,7 @@ module.exports = (app) => {
         cookie: {
             maxAge: 1000 * 60 * 60, // 1 hora
             httpOnly: true, // Acessível apenas via HTTP
-            secure: false,
+            secure: process.env.NODE_ENV === 'production', // Apenas em produção
             sameSite: 'lax' // Protege contra CSRF
         }
     }));
