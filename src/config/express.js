698959654c98a4e6ebd3aa705747c65b2808b12c
Fabricio <fabricio.bizotto@ifc.edu.br>
2025-09-16 13:21:10 -0300

    asd

--- src/config/express.js
+++ src/config/express.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const path = require("path");
-const isProduction = process.env.NODE_ENV === "production";
 
 const app = express();
-
-if (isProduction) {
-    app.set("trust proxy", 1); // Trust first proxy
-}
 
 // Middlewares básicos
 app.use(express.json());
