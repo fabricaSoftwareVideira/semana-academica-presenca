403dbddaadb39bee3a3b25e97affc67e96406ea8
Fabricio <fabricio.bizotto@ifc.edu.br>
2025-09-16 21:30:44 -0300

    asd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
             name: 'semana-academica',
             script: 'server.js',
             instances: 'max',
-            exec_mode: 'cluster',
+            exec_mode: 'fork', // Usar 'fork' para evitar problemas com bibliotecas que não suportam cluster
             env: {
                 NODE_ENV: process.env.NODE_ENV || 'development',
                 PORT: process.env.PORT || 3000
