e19cb58f7ad404844ba9e6870fb4418795e8f1e9
Fabricio <fabricio.bizotto@ifc.edu.br>
2025-09-16 21:44:13 -0300

    log

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,10 @@ module.exports = {
             env: {
                 NODE_ENV: process.env.NODE_ENV || 'development',
                 PORT: process.env.PORT || 3000
-            }
+            },
+            out_file: './logs/out.log',
+            error_file: './logs/error.log',
+            log_date_format: 'YYYY-MM-DD HH:mm Z'
         }
     ]
 };
